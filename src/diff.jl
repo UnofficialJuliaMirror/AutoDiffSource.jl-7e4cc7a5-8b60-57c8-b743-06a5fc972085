9b6b543338902fe10a5a7bdce2f831ad26dee56b
Joe Petviashvili <joe@gnom.us>
2016-11-02 22:02:35 -0700

    forward pass

--- src/diff.jl
+++ src/diff.jl
@@ -3,6 +3,25 @@ macro δ(expr)
 end
 
 function δ(ops)
-    @show ops
-    :()
+    name = Symbol("δ$(ops.name)")
+    func = :(function $name($(ops.inputs...)); end)
+    body = func.args[2].args
+    empty!(body)
+    info = Expr(:line)
+    for line in ops.body
+        if info != line.info
+            info = line.info
+            push!(body, info)
+        end
+        nabla = Symbol("∇$(line.name)_$(line.outputs[1])")
+        name = Symbol("δ$(line.name)")
+        push!(body, :(($(line.outputs...), $nabla) = $name($(line.inputs...))))
+    end
+    push!(body, ∇(ops))
+    push!(body, :($(ops.outputs...), $(Symbol("∇$(ops.name)"))))
+    @show func
+    func
+end
+
+function ∇(ops)
 end
