412d1a74ddedf649a973e6c7e242a84dddbc51ac
Joe Petviashvili <joe@gnom.us>
2016-11-02 23:11:54 -0700

    reverse pass first draft

--- src/diff.jl
+++ src/diff.jl
@@ -24,4 +24,51 @@ function δ(ops)
 end
 
 function ∇(ops)
+    name = Symbol("∇$(ops.name)")
+    inputs =  [Symbol("∂$x") for x in ops.outputs]
+    func = :(function $name($(inputs...)); end)
+    body = func.args[2].args
+    empty!(body)
+    info = Expr(:line)
+    partials = Set(inputs)
+    temp = 1
+    for line in reverse(ops.body)
+        if info != line.info
+            info = line.info
+            push!(body, info)
+        end
+        nabla = Symbol("∇$(line.name)_$(line.outputs[1])")
+        i = [Symbol("∂$x") for x in line.outputs]
+        o = [Symbol("∂$x") for x in line.inputs]
+
+        inter = intersect(partials, Set(o))
+        if length(inter) > 0 # XXX or same variable is used twice in the inputs
+            if length(o) == 1
+                push!(body, :($(to_tuple_or_expr(o)) += $nabla($(i...))))
+            else
+                oo = copy(o)
+                for k in 1:length(o)
+                    if in(o[k], inter)
+                        oo[k] = Symbol("temp$temp")
+                        temp += 1
+                    end
+                end
+                push!(body, :($(to_tuple_or_expr(oo)) = $nabla($(i...))))
+                for k in 1:length(o)
+                    if in(o[k], inter)
+                        push!(body, :($(o[k]) += $(oo[k])))
+                    end
+                end
+            end
+        else
+            push!(body, :($(to_tuple_or_expr(o)) = $nabla($(i...))))
+        end
+        [push!(partials, e) for e in o]
+    end
+
+    outputs = [Symbol("∂$x") for x in ops.inputs]
+    push!(body, to_tuple_or_expr(outputs))
+    func
 end
+
+to_tuple_or_expr(o) = length(o) == 1 ? o[1] : Expr(:tuple, o...)
