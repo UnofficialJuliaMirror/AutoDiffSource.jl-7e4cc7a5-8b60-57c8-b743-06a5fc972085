a46277df9cb9a82f1e4d870585770ee96a916850
Joe Petviashvili <joe@gnom.us>
2016-11-23 14:43:11 -0800

    support simple broadcast

---
+++ src/multicast.jl
@@ -0,0 +1,67 @@
+δmulticast(f, x::AbstractFloat) = f(x)
+
+function δmulticast(f, x::AbstractArray)
+    iter = Base.CartesianRange(size(x))
+    temp = next(iter, start(iter))
+    I = temp[1]
+    st = temp[2]
+    val = f(x[I])
+    v = similar(Array{typeof(val[1])}, size(x))
+    ∇v = similar(Array{typeof(val[2])}, size(x))
+    v[I] = val[1]
+    ∇v[I] = val[2]
+    while !done(iter, st)
+        I, st = next(iter, st)
+        v[I], ∇v[I] = f(x[I])
+    end
+    v, u -> ∇multicast(∇v, u)
+end
+
+function ∇multicast(∇v, x)
+    iter = Base.CartesianRange(size(x))
+    st = start(iter)
+    I, st = next(iter, st)
+    val = ∇v[I](x[I])
+    v = similar(Array{typeof(val)}, size(x))
+    v[I] = val
+    while !done(iter, st)
+        I, st = next(iter, st)
+        v[I] = ∇v[I](x[I])
+    end
+    v
+end
+
+δmulticast(f, x1::AbstractFloat, x2::AbstractFloat) = f(x1, x2)
+
+function δmulticast(f, x1::AbstractArray, x2::AbstractArray)
+    iter = Base.CartesianRange(size(x1))
+    temp = next(iter, start(iter))
+    I = temp[1]
+    st = temp[2]
+    val = f(x1[I], x2[I])
+    v = similar(Array{typeof(val[1])}, size(x1))
+    ∇v = similar(Array{typeof(val[2])}, size(x1))
+    v[I] = val[1]
+    ∇v[I] = val[2]
+    while !done(iter, st)
+        I, st = next(iter, st)
+        v[I], ∇v[I] = f(x1[I], x2[I])
+    end
+    v, u -> ∇multicast2(∇v, u)
+end
+
+function ∇multicast2(∇v, x)
+    iter = Base.CartesianRange(size(x))
+    st = start(iter)
+    I, st = next(iter, st)
+    val = ∇v[I](x[I])
+    v1 = similar(Array{typeof(val[1])}, size(x))
+    v2 = similar(Array{typeof(val[2])}, size(x))
+    v1[I] = val[1]
+    v2[I] = val[2]
+    while !done(iter, st)
+        I, st = next(iter, st)
+        v1[I], v2[I] = ∇v[I](x[I])
+    end
+    v1, v2
+end
